2da1e83258837d294ebc65a8b7856ff567391e9f
Михаил Бабкин <Shar170@yandex.ru>
2021-10-18 00:35:24 +0300

    add more filters!

--- siteMap.py
+++ siteMap.py
@@ -4,9 +4,10 @@ import pydeck as pdk
 import shapefile
 import streamlit as st
 
+#инициализация параметров страницы
 st.set_page_config(
     page_title="KOD в мешке App",
-    page_icon="😻",
+    page_icon="🐱",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
@@ -15,7 +16,7 @@ st.set_page_config(
 )
 
 
-
+#функция чтения shape файла с размерами и координатами сетки райнов
 def read_shapefile(sf_shape):
     """
     Read a shapefile into a Pandas dataframe with a 'coords' 
@@ -55,10 +56,6 @@ def load_h_w():
 #@st.cache
 def load_names():
     #считываем наименование всех регинов и прикручиваем к ним их координаты
-    #loc_names = pd.read_excel('names.xlsx')
-    #loc_names['cell_zid'] = loc_names['cell_zid'].map(int)
-    #loc_names['adm_zid'] = loc_names['adm_zid'].map(int)
-    #loc_names.to_csv('rebuilded_names.csv')
     loc_names = pd.read_csv('rebuilded_names.csv')
     return loc_names.merge(right=s_df, how='inner', left_on='cell_zid', right_on='cell_zid')
 
@@ -66,65 +63,118 @@ def load_names():
 def load_loc_info():
     #считываем информацию о плотности проживающего, работающего и проходящего населения для каждого квадрата и прикручиваем туда координаты квадратов
     c_locations = pd.read_csv("june_full_data.csv")
-    # c_locations = c_locations.merge(right=s_df, how='inner', left_on='zid', right_on='cell_zid')
-    # c_locations.drop('cell_zid', axis=1, inplace=True)
-    # c_locations['lat'] = c_locations['coords'].apply(lambda x: x[0][1]) # извлекаем широту
-    # c_locations['lon'] = c_locations['coords'].apply(lambda x: x[0][0]) # извлекаем долготу
-    # c_locations.drop('coords', axis=1, inplace=True)
     return c_locations
 @st.cache
 def get_unic_names():
     return  pd.read_csv('rebuilded_names.csv')['adm_name'].drop_duplicates(inplace=False).values
 
 
+def load_mfc_info():
+    #считываем информацию о плотности проживающего, работающего и проходящего населения для каждого квадрата и прикручиваем туда координаты квадратов
+    mfc = pd.read_csv("mos_coords.csv")
+    return mfc
 
-
-loc_names = load_names()# pd.DataFrame.copy() #.copy(deep=True)
+#инициализируем и подгружаем все датасеты
+loc_names = load_names()
 c_locations = load_loc_info()
-h_w_matrix = load_h_w()
-adm_names = get_unic_names()#loc_names['adm_name'].drop_duplicates(inplace=False).values
+#h_w_matrix = load_h_w()
+adm_names = get_unic_names()
+mfc_info_df = load_mfc_info()
 
+#st.dataframe(mfc_info_df.head())
+
+st.sidebar.image('whiteCat.png', width=100)
 st.title('Проект команды "KOD в мешке"')
-st.write('Карта Москвы и МО. Красные круги отображают выбранный вами район')
+
+st.write('Карта Москвы и МО. Круги отображают выбранный вами район')
 b_types_array = ['МФЦ','Школа','Торговый центр']
 
 adm_zone = st.sidebar.selectbox('Выберите административную зону',adm_names, )
+print_all_btn = st.sidebar.checkbox('Вывести для всех регионов', value=False)
 build_type = st.sidebar.selectbox('Выберите тип учреждения',b_types_array)
+show_mfc = st.sidebar.checkbox('Показать учреждения на карте', value=False)
 filter_dict = {'Кол-во живущих':'home','Кол-во работающих':'job','Кол-во проходящях':'move','Логистика':'logistic'}
-filter_type = st.sidebar.selectbox('Выберите фильтр',filter_dict)
-filter_value = st.sidebar.slider('Выберите степень влияния', value=0.5, min_value=0.0, max_value=5.0)
+filter_type = st.sidebar.selectbox('Выберите оценочный фактор',filter_dict)
+filter_value = st.sidebar.slider('Выберите степень влияния фактора', value=0.5, min_value=0.0, max_value=5.0)
 #press_button = st.sidebar.button("Do magic!")
 
 filter_key = filter_dict[filter_type]
 
 c_locations['mfc_chance'] = 0
 
-#Настройка влияния весов на параметры плотности людей
+#Настройки влияния весов на параметры плотности людей
 alphas = {'home':1.0,'job':1.0,'day':1.0, 'move':1.0}
 alphas_dlt = {'home':0.5,'job':0.5,'day':0.5, 'move':0.5}
 
+#Расчитываем оценку необходимости постройки учреждения
+for feature in ['home', 'job', 'day', 'move']:#обрабатываем счётчики плотности за текущий месяц
+    c_locations['mfc_chance'] = c_locations['mfc_chance'] + (filter_value if filter_key == feature else (1.0)) * alphas[feature] * c_locations[f'customers_cnt_{feature}']
+for feature in ['home', 'job', 'day', 'move']:#обрабатываем дельты измения плотностей
+    c_locations['mfc_chance'] = c_locations['mfc_chance'] + (filter_value if filter_key == feature else  (1.0)) * alphas_dlt[feature] *  c_locations[f'customers_dlt_{feature}']  
 
-for feature in ['home', 'job', 'day', 'move']:
-    c_locations['mfc_chance'] = c_locations['mfc_chance'] + (filter_value if filter_key == feature else 1.0) * alphas[feature] * c_locations[f'customers_cnt_{feature}']
-for feature in ['home', 'job', 'day', 'move']:
-    c_locations['mfc_chance'] = c_locations['mfc_chance'] + (filter_value if filter_key == feature else 1.0) * alphas_dlt[feature] *  c_locations[f'customers_dlt_{feature}']  
-    
-c_locations['mfc_chance'] = c_locations['mfc_chance'] + (c_locations['nearest_mfc'])# / 43617.48364582916)*1000
-c_locations['mfc_chance'] = c_locations['mfc_chance'] + (filter_value if filter_key == 'logistic' else 1.0) * (c_locations['logistic'])
-
+#инкрементация шанса за счёт большой удалённости от других учреждений
+c_locations['mfc_chance'] = c_locations['mfc_chance'] + (c_locations['nearest_mfc'])
+#инкрементация шанс за счёт высокой логичтики внутри ячейки
+c_locations['mfc_chance'] = c_locations['mfc_chance'] + (filter_value if filter_key == 'logistic' else  (1.0)) * (c_locations['logistic'])
+#нормирование шанса 
 c_locations['mfc_chance'] = c_locations['mfc_chance'].apply(lambda x: 1 + 10* x / 42070.344117)
 
-df = c_locations.loc[c_locations['zid'].isin(loc_names.loc[loc_names['adm_name'] == adm_zone]['cell_zid'])]
+#извлекаем ячейки выбранного в меню района Москвы
+if print_all_btn:
+    df = c_locations.copy()#.loc[c_locations['zid'].isin(loc_names.loc[loc_names['adm_name'] == adm_zone]['cell_zid'])]
+else:
+    df = c_locations.loc[c_locations['zid'].isin(loc_names.loc[loc_names['adm_name'] == adm_zone]['cell_zid'])]
 
-st.write(f'''Вы выбрали район "{adm_zone}" сейчас население в нём: {sum(df["customers_cnt_home"].values)}чел. на { df.shape[0]*0.25} км²
+#Выводим описание выьранного региона (население, площадь) и информацию 
+st.write(f'''Вы выбрали район "{adm_zone}" сейчас население в нём: {sum(df["customers_cnt_home"].values) + sum(df["customers_cnt_move"].values)}чел. на { df.shape[0]*0.25} км²
             Вы выбрали фильтрацию по учреждению типа: "{build_type}" с коэффициентом влияния равным {filter_value}
             с учётом того что это будет {filter_type.lower()}.
 ''')
+#Собираем шаблон подсказки для столбцов(ячеек) карты
 if filter_key != 'logistic':
     tooltip_template = "<b>" + filter_type + " :</b> {customers_cnt_"+filter_key+"} <br/><b>Необходисоть постройки "+build_type+" :</b> {mfc_chance} <br/>"
 else:
     tooltip_template = "<b>" + filter_type + " :</b> {logistic} <br/><b>Необходисоть постройки "+build_type+" :</b> {mfc_chance} <br/>"
 
+layers=[
+        pdk.Layer("ColumnLayer", #слой для отображения колонок вероятности постройки учреждения
+        data=df,
+        get_position='[lon,lat]',
+        get_elevation="mfc_chance",
+        elevation_scale=100,
+        radius=250,
+        get_fill_color=["mfc_chance * 21","255-mfc_chance*21",1, 200],
+        pickable=True,
+        auto_highlight=True,
+        ),
+        pdk.Layer(
+            "TextLayer",
+            data=mfc_info_df,
+            get_position='[lon,lat]',
+            #get_text="mfc_chance",
+            text='МФЦ',
+            #elevation_scale=100,
+            #radius=250,
+            pickable=False,
+            auto_highlight=True,
+        )]
+
+if show_mfc:
+    layers.append(pdk.Layer("ColumnLayer", #слой для отображения уже существующих МФЦ
+        data=mfc_info_df if print_all_btn else mfc_info_df.loc[adm_zone.lower() == mfc_info_df['District'].str.lower() ],
+        get_position='[lat,lon]',
+        elevation=100,#"WindowCount",
+        elevation_scale=100,
+        radius=50,
+        get_fill_color=[1,1,255, 200],
+        pickable=True,
+        auto_highlight=True,    
+        ))
+
+
+
+
+#Определяем графики на карте для районов и известных учреждений
 world_map = pdk.Deck(
     map_style='mapbox://styles/mapbox/light-v9',
     initial_view_state=pdk.ViewState(latitude=df['lat'].values[0],longitude=df['lon'].values[0],zoom=11,pitch=50,),
@@ -132,26 +182,12 @@ world_map = pdk.Deck(
         "html": tooltip_template,
         "style": {"backgroundColor": "black","color": "white"}
     },
-    layers=[
-        pdk.Layer("ColumnLayer",
-        data=df,
-        get_position='[lon,lat]',
-        get_elevation="mfc_chance",
-        elevation_scale=100,#max(c_locations['customers_cnt_home'].values)/max(df['customers_cnt_home'].values),
-        radius=250,
-        get_fill_color=["255-mfc_chance * 21","mfc_chance*21",1, 200],
-        pickable=True,
-        auto_highlight=True,    
-    )
-        #pdk.Layer('ScatterplotLayer',data=df,get_position='[lon,lat]',get_color='[200, 30, 0, 160]',get_radius=250,)
-        #pdk.Layer("ScreenGridLayer",df,pickable=False,opacity=0.8,cell_size_pixels=5,color_range=[[0, 25, 0, 25],[0, 85, 0, 85],[0, 127, 0, 127],[0, 170, 0, 170],[0, 190, 0, 190],[0, 255, 0, 255],],get_position='[lon,lat]',get_weight=10,)
-    ],
+    layers=layers,
 )
 
 
 
-success_box = st.success(f'Выберите ячейку на карте')
-
+success_box = None
 def handle_on_click(widget_instance, payload):
     global success_box
     try:
@@ -159,8 +195,6 @@ def handle_on_click(widget_instance, payload):
         coords = payload['data']['coordinate']
     except Exception as e:
         success_box.body = 'Error: %s' % e
-
-
 world_map.deck_widget.on_click(handle_on_click)
 
 map_widget = st.pydeck_chart(world_map)
